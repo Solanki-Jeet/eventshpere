cf43a227127b54d3ca00d216d042d95bea8f77f4
Jeet Solanki <jeetsolanki9989@gmail.com>
2026-09-16 22:58:28 +0530

    Initial commit: EventSphere full-stack project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { store } from './store'
+import App from './App.jsx'
+import './index.css'
+
+// Create a client for React Query caching
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+})
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+)
